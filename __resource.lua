ebc87104d0b7c7ff0fc8b9f77e8ee2df3935b35b
麻辣戈壁 <lpj.vip@foxmail.com>
2019-03-20 20:22:44 +0800

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -10,6 +10,8 @@ server_scripts {
 client_scripts {
     '@es_extended/locale.lua',
     'locales/en.lua',
+    'locales/cn.lua',
+    'locales/zh.lua',
     'config.lua',
     '@es_extended/i18n.lua',
     'client/main.lua'
